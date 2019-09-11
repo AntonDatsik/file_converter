37d8662f7c577b02b3acbbbcbad71c121e9b8b1b
Anton Dacik <anton.dacik@gmail.com>
2019-09-11 20:49:10 +0300

    Added fonts install to Dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM python:3.7-alpine
+FROM python:3.7.4-alpine
 
-RUN apk add --no-cache  --virtual .build-deps \
-    postgresql-dev \
-    gcc musl-dev jpeg-dev zlib-dev libffi-dev \
-    cairo-dev pango-dev gdk-pixbuf-dev
+RUN apk --update --upgrade add gcc \
+    musl-dev jpeg-dev zlib-dev libffi-dev cairo-dev pango-dev \
+    font-noto \
+    gdk-pixbuf-dev postgresql-dev
 
 WORKDIR /app
 
--- file_converter/settings/__init__.py
+++ file_converter/settings/__init__.py
@@ -1,19 +1,13 @@
-import os
-
 from .apps import INSTALLED_APPS
 from .auth_user import *
 from .base import *
 from .databases import DATABASES
-from .middlewares import MIDDLEWARE
-from .templates import TEMPLATES
-from .rest_framework import REST_FRAMEWORK
 from .logging import LOGGING
+from .middlewares import MIDDLEWARE
+from .rest_framework import REST_FRAMEWORK
+from .templates import TEMPLATES
 
-
-settings_local = os.environ.get(
-    'PDF_GENERATOR_SETTINGS_LOCAL_PATH',
-    os.path.join(BASE_DIR, 'settings_local.py'),
-)
-
-if os.path.exists(settings_local):
-    exec(open(settings_local).read())
+try:
+    from settings_local import *
+except ImportError:
+    pass
